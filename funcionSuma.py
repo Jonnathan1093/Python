5ca307260801c178ea5ff7f4fce4ab0722a9b8a3
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-06-22 16:03:42 -0500

    Ejemplos de Funciones

---
+++ funcionSuma.py
@@ -0,0 +1,11 @@
+def suma (num1, num2):
+    resultado = num1 + num2
+    return resultado
+num1 = int(input("Dime el primer numero "))
+num2 = int(input("Dime el segundo numero "))
+
+#Llamada a la funcion
+resultado = suma(num1, num2)
+#Mostramos resultado
+#print ("La suma es igual a "+ str(resultado))
+print (f"La suma es igual a {resultado}")
--- funciones.py
+++ funciones.py
@@ -1,8 +1,22 @@
 def esPar(numero):
-    if numero%2==0:
-        print("El numero es par")
+    if numero % 2 == 0:
+        #print("El numero es par")
+        return True
     else:
-        print("El numero no es impar")
-        
-numero=int(input("Dime un numero y te dire si es par o impar "))
-esPar(numero)
+        #print("El numero no es impar")
+        return False
+    
+numero=int(input("Dime un numero y te dire si es par o impar "))#Llamamos al usuario
+resultado = esPar(numero) #Llamamos a la funcion
+if resultado:
+    print(f"El numero {numero} es par")
+else:
+    print(f"El numero {numero} es impar")
+
+
+
+
+
+
+
+
