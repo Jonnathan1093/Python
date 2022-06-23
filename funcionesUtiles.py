f59741b182f211ef45a14c6bab143c6e00eedc04
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-06-23 16:12:34 -0500

    Funciones utiles

---
+++ funcionesUtiles.py
@@ -0,0 +1,19 @@
+#Redondeo
+print (round(3.4568))
+#Valor absoluto
+print (abs(-56))
+#Crear una lista
+lista = [1,8,85,62,74,7,52,41,6,3,47,4]
+print (lista)
+#Minimo de la lista
+print (min(lista))
+#Maximo de la lista
+print (max(lista))
+#Ordemar
+print (sorted(lista))
+#Suman total de la lista
+print (sum(lista))
+
+
+
+
