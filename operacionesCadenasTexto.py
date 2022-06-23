8d90f8faf757cffc71995740cd2654aee69222a6
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-06-23 17:19:16 -0500

    Operadores para cadenas de texto

---
+++ operacionesCadenasTexto.py
@@ -0,0 +1,28 @@
+cadena = "EjEmpLO De CAdeNa dE tExtOS"
+#Convertir a minusculas
+print (cadena.lower())
+#Convertir a mayusculas
+print (cadena.upper())
+#Primera letra mayuscula
+print (cadena.capitalize())
+#Para poner a cada letra de una oracion con mayuscula usamos title
+print (cadena.title())
+#Invertir las letras de la cadena dependiendo como se escribio
+print (cadena.swapcase())
+#Esta nos dira si es un booleano Verdadero o falso de Mayusculas o minusculas
+cadena ="MAYUSCULA"
+print (cadena.isupper())
+#Esta nos dira si es la cadena es minuscula
+cadena ="minuscula"
+print (cadena.islower())
+#Comprobar si la cadena es numerica
+cadena = "123"
+print (cadena.isnumeric())
+#Comprobar si tiene letras y numeros, no caracteres extraños "Alfanumerica"
+cadena = "#$)"
+print (cadena.isalnum())
+#Comprobar si es un titulo
+cadena = "esto no es un titulo"
+print (cadena.istitle())
+
+
