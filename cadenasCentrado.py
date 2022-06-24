84495a56805c6051eaea4744d2cfad9e577c4c3e
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-06-24 15:11:01 -0500

    Reemplazar y centrar cadenas de texto

--- cadenasCentrado.py
+++ cadenasCentrado.py
@@ -1,2 +1,33 @@
-texto = "Esto es un texto para el ejemplo que vamoss a realizar "
-print ()
+texto = "Esto es un texto para el ejemplo que vamoss a realizar"
+#startswith para buscar la primera palabra
+print ("La cadena empieza con: ", texto.startswith("vamos"))
+#endswith para buscar la ultima palabra
+print ("La cadena termina con: ", texto.endswith("realizar"))
+#El lower para que combierta en minuscula y hacer una comparacion en este ejemplo
+print ("La cadena termina con: ", texto.lower().endswith("Realizar".lower()))
+
+#Alinear texto centrado
+print (texto.center(80, '*'))
+# Para contar los caracteres
+longitudCadena = len(texto)
+print (longitudCadena)
+print (texto.center(longitudCadena+7, '-'))
+
+#Texto a la izquierda
+print (texto.ljust(80, '-'))
+
+# Texto a la derecha
+print (texto.rjust(80,'*'))
+
+# Emilimar espacios en blanco
+texto = "   Esta es una oracion donde se elimina algunos espacios -l/* -/      "
+print (texto)
+print ("Cadena sin espacios en blanco: ")
+print (texto.strip())
+
+# Sustituir caracteres
+print (texto.replace("-", " hola "))
+# Esta es una manera de hacerlo pero guardandolo
+textoModificado = texto.replace("-", " hola ") 
+print(textoModificado)
+print(texto)
