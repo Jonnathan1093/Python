29ed564a0dff62588181be4b736df9aa42c19d80
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-06-27 22:27:17 -0500

    modulo math

---
+++ moduloMath.py
@@ -0,0 +1,9 @@
+# Modulo de mathematicas
+import math
+# Si queremos importat algo mas concreto debemos hacerlo
+# from math import sqrt (Esto nos importaria solo la funcion "sqrt" dentro del modulo "math") 
+numero = int(input("Dime un numero y te difo su raiz "))
+print("La raiz cuadrada de " + str(numero) + " es " + str(math.sqrt(numero)))
+
+
+
