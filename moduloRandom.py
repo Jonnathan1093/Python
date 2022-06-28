a8f0d48e8ba76ba7d7d81cf078ab65ba0ffe77ed
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-06-27 22:47:45 -0500

    modulo rando ejemplo 2

--- moduloRandom.py
+++ moduloRandom.py
@@ -1,7 +1,8 @@
-from random import randint as azar
+# from random import randint as azar
+from random import *
 continua = "s"
 while(continua=="s" or continua=="S"):
-    lanzaDado = azar(1,8)
+    lanzaDado = randint(1,8)
     print("Haz sacado un "+str(lanzaDado))
     continua = input("Continuamos (s/n)")
 print("Ya no hay mas lanzamientos")
