a7f127e616776ecf90919025472ca989062f4145
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-06-30 17:06:29 -0500

    Archivo para instalar librerias a otro proyecto

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name ="miFuncion",
+    version ="1.0",
+    description ="realizar sumas y comprobar si esta impar",
+    author = "Jonnathan Heras",
+    author_email ="jonnathan1093@gmail.com",
+    url = "https://retrogeeksla.blogspot.com/",
+    packages=["paquetes"]
+)
+
+
+
+
+
+
