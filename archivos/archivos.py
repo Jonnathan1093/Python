2914cd2db3c515de7b375c8adf12e6007949c8d1
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-07-15 16:11:41 -0500

    Carpeta archivos, y renombrar, eliminar archvos Finalizado

--- archivos/archivos.py
+++ archivos/archivos.py
@@ -19,20 +19,52 @@ print(filtrado)
 # Cambio de directorio
 os.chdir("D:\\PYTHON\\Curso Python\\archivos\\micarpeta")
 # Como renombrar los archivos
-os.rename("CAPTURAS INFORME - copia (2).rar","Captura informe.zip")
+# os.rename("CAPTURAS INFORME - copia (2).rar","Captura informe.zip")
 
+# Eliminar o borrar un archivo
+os.chdir("D:\\PYTHON\\Curso Python\\archivos\\micarpeta")
+# os.remove("Captura informe.rar")
+filtrado = [archivo for archivo in listado if archivo.endswith(".rar")]
+print(filtrado)
 
+""" # Renombrar varios archivos a la vez
+contador = 1
+listado = os.listdir(carpeta)
+print(listado)
+print("Fin listado inicial")
+for archivo in os.listdir():
+    nombre, extension = os.path.splitext(archivo)
+    print(nombre)
+    print(extension)
+    nuevoNommbre = f'renombrado{str(contador)}_{nombre}{extension}'
+    contador+= 1
+    os.rename(archivo, nuevoNommbre)
+listado = os.listdir(carpeta)
+print("Listado renombrado")
+print("\n\n")
+print(listado) """
 
+""" # Copiar contenido de un fichero a otro
+try:
+    fichero = open("testNoexiste.txt", "r")
+    nuevoFichero = open("nuevoTest.txt", "w")
+    os.chdir("D:\\PYTHON\\Curso Python\\archivos\\micarpeta")
+    for linea in fichero:
+        nuevoFichero.write(linea)
+    fichero.close()
+    nuevoFichero.close()
+except FileNotFoundError:
+    print("No se ha encontrado el fichero") """
 
-
-
-
-
-
-
-
-
-
+# Otra forma de copiar un archivo a otro con "with"
+try:
+    os.chdir("D:\\PYTHON\\Curso Python\\archivos\\micarpeta")
+    with open("testNoExisto.txt") as fichero:
+        with open("nuevoTest.txt", "w") as nuevoFichero:
+            for linea in fichero:
+                nuevoFichero.write(linea)
+except FileNotFoundError:
+    print("Fichero no encontrado")
 
 
 
