457d8d19b59f340cd1856e50a31a73fa44d7db4d
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-07-16 11:49:09 -0500

    Concepto de Rutas

---
+++ rutas/rutas.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+# Directorio actual
+directorio_actual = os.getcwd()
+print(directorio_actual)
+# directorio datos
+directorio_datos = os.path.join(directorio_actual,'datos')
+print(directorio_datos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
