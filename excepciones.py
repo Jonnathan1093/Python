4bb1bbd3750a0fa47268de3db60be27b16461ab4
Jonnathan Heras <jonnathan1093@yahoo.com>
2022-08-06 23:30:59 -0500

    Manera de como documentar

--- excepciones.py
+++ excepciones.py
@@ -1,16 +1,16 @@
 import os
-# # Error de la divicion x 0
-# numero1 = 45
-# numero2 = 0
-# try:
-#     resultado = numero1 / numero2
-#     print(resultado)
-# except:
-#     resultado = 0
-#     print("Ha ocurrido un error")
-# finally:
-#     print(resultado)
-#     print("Esto se ejecuta siempre") 
+# Error de la divicion x 0
+numero1 = 45
+numero2 = 0
+try:
+    resultado = numero1 / numero2
+    print(resultado)
+except:
+    resultado = 0
+    print("Ha ocurrido un error")
+finally:
+    print(resultado)
+    print("Esto se ejecuta siempre") 
 try:
     os.remove(os.getcwd()+"/archivo10.txt")
 except FileNotFoundError:
---
+++ test.py
@@ -0,0 +1,14 @@
+def multiplicar(num1, num2):
+    """"
+    Funsion que multiplica dos numeros
+    numero 1 (int)
+    numero 2 (int)
+    Retorna la multiplicacion de los parametros dados    
+    
+    >>> multiplicar(2,3)
+    6
+    >>> multiplicar(2,4)
+    9
+    """
+    return num1 * num2
+print(f"El resultado de multiplicar es {multiplicar(6,12)}")
